67861b2e1c142614e8b203b96648c61beb2b8e09
Tao Xie <taoxie1025@gmail.com>
2016-11-07 07:37:56 +0000

    sign in page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,6 @@ class UsersController < ApplicationController
         end
     end
     
-    
     def show
         
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   resource :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -10,6 +11,9 @@ Rails.application.routes.draw do
   get 'welcome/about', to: 'welcome#about'
   get 'users/new', to: 'users#new'
   post 'users/' => 'users#show', :as => 'user'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
