310749bc5f8d77ec19bb692114a8d2957516f720
Tao Xie <taoxie1025@gmail.com>
2016-11-08 00:20:08 +0000

    update header and log in page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            # handle successful save
+            log_in @user
             flash[:success] = "Welcome to the Sample App!"
             redirect_to @user
         else
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,11 @@ class User < ActiveRecord::Base
                                 uniqueness: { case_sensitive: false}
     has_secure_password
     validates :password, length: { minimum: 6 }, allow_nil: true
+    
+    def User.digest(string)
+        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+     BCrypt::Password.create(string, cost: cost)
+  end
   
 end
