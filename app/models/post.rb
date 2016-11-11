8c4a9a248a21a7936c42dc51964c5a688e720306
Anuja <anuja.488@gmail.com>
2016-11-11 02:30:17 +0000

    Fixed Search on course code and department

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,11 +9,16 @@ class Post < ActiveRecord::Base
     validates :description, presence: true, length: { maximum: 140 }
     
     def self.search(search)
-     where("courseName LIKE ?", "%#{search}") 
+     if search
+      where("courseName LIKE ? OR courseCode LIKE ? OR department LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%")
+    #  where("courseName LIKE ?", "%#{search}%")
     #  where("courseCode LIKE ?", "%#{search}%") There is problem here, it always returns the last line
     #  where("department LIKE ?", "%#{search}%") which is empty. We need to figure out a way to do such query
     # and watch out the lowercase and uppercase. Right now id doesn't work for one work search, you have to search
     # the whole word -> Database System, not Database
+     else
+      scoped
+     end
     end
     
 end
