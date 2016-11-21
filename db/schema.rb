5ab423973993e3c2d2154e6d9bbd021dd5f2c892
Tao Xie <taoxie1025@gmail.com>
2016-11-21 04:27:37 +0000

    Added isAdmin to Users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161113175524) do
+ActiveRecord::Schema.define(version: 20161121040648) do
 
   create_table "feedbacks", force: :cascade do |t|
     t.text     "comment"
@@ -98,9 +98,10 @@ ActiveRecord::Schema.define(version: 20161113175524) do
     t.string   "name"
     t.string   "password"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "password_digest"
+    t.boolean  "isAdmin",         default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
