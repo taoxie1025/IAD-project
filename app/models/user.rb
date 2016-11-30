f8f2e0026de0b66c4965a916219f088880a87017
Tao Xie <taoxie1025@gmail.com>
2016-11-30 19:36:30 +0000

    Adding message image and new message notification

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ActiveRecord::Base
     
     acts_as_messageable
     
-    has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150#" }
-     validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+    has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150#" }, :default_url => ActionController::Base.helpers.asset_path('user_profile_placeholder.png')
+    validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
      
     has_many :posts, dependent: :destroy
 
